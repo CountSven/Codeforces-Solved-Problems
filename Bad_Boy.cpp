ab4ef90a39cc886ee5c96631365de795b53f09b9
Abdullah Al Sakib <abdullahalsakib001@gmail.com>
2025-06-30 19:10:06 +0600

    Add files via upload

---
+++ Bad_Boy.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t;
+	cin >> t;
+
+	while ( t-- ) {
+		int n, m, i, j;
+		cin >> n >> m >> i >> j;
+
+		cout << 1 << " " << 1 << " " << n << " " << m << "\n"; 
+	}
+
+	return 0;
+}

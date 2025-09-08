0c4dacdf3146cc9808a95d7433e3d1b91b3ccf5f
Abdullah Al Sakib <abdullahalsakib001@gmail.com>
2025-09-08 23:57:41 +0600

    Add files via upload

---
+++ Helmets_in_Night_Light.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t;
+	cin >> t;
+
+	while ( t-- ) {
+		int n, p;
+		cin >> n >> p;
+		vector<pair<int, int>> v(n);
+
+		for ( int i = 0; i < n; i++ ) cin >> v[i].second;
+		for ( int i = 0; i < n; i++ ) cin >> v[i].first;
+
+		sort( v.begin(), v.end() );
+
+		for ( int i = 0; i < n; i++ ) v[i].first = min( v[i].first, p );
+
+		int cur = 1, cnt = p;
+
+		for ( int i = 0; i < n; i++ ) {
+			int now = min( cur + v[i].second, n );
+			
+			cnt += ( ( now - cur ) * v[i].first );
+
+			cur = now;
+		}
+
+		cout << cnt << "\n";
+	}
+
+	return 0;
+}

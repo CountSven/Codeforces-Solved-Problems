b9e270148ee2b6170d25a1b62dadc62c673fef11
CountSven <abdullahalsakib001@gmail.com>
2025-11-01 23:58:43 +0600

    Add files via upload

---
+++ The_Fortune_Dice.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int x;
+	cin >> x;
+
+	if ( x >= 2 && x <= 12 ) cout << "YES" << "\n";
+	else cout << "NO" << "\n";
+
+	return 0;
+}

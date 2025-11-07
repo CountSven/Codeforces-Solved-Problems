ef430b09f92d8d7c17620fd9f7ee798d705c00e7
CountSven <abdullahalsakib001@gmail.com>
2025-11-07 20:00:20 +0600

    Add files via upload

---
+++ An_Easy_Math_Problem.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t;
+	cin >> t;
+
+	while ( t-- ) {
+		string s;
+		cin >> s;
+
+		int sum = 0;
+
+		for ( int i = 0; i < s.size(); i++ ) sum += ( s[i] - '0' );
+
+		if ( sum % 9 == 0 ) cout << "Yes" << "\n";
+		else cout << "No" << "\n";
+	}
+
+	return 0;
+}

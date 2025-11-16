9884b787087753ebfc8f242787526eea76ff2112
CountSven <abdullahalsakib001@gmail.com>
2025-11-16 20:30:45 +0600

    Add files via upload

---
+++ Interesting_drink.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+	vector<int> v(n);
+
+	for ( int i = 0; i < n; i++ ) cin >> v[i];
+
+	sort( v.begin(), v.end() );
+
+	int q;
+	cin >> q;
+
+	while ( q-- ) {
+		int val;
+		cin >> val;
+
+		int idx = upper_bound( v.begin(), v.end(), val ) - v.begin();
+
+		cout << idx << "\n";
+	}
+
+	return 0;
+}

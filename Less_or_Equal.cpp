0dcec5c5bf6f1d42ca43dd6ec9d6183461f68972
CountSven <abdullahalsakib001@gmail.com>
2026-01-14 21:43:52 +0600

    Add files via upload

--- Less_or_Equal.cpp
+++ Less_or_Equal.cpp
@@ -14,8 +14,24 @@ int main()
 
 	sort( a, a+n );
 
-	for ( auto u : a ) cout << u << " ";
-	cout << "\n";
+	int l = 0, cnt = 0, val = 1;
+
+	while ( l < n && a[l] == val ) {
+		cnt++;
+		l++;
+	}	
+	while ( l < n && cnt < k ) {
+		cnt++;
+		while ( l+1 < n && a[l+1] == a[l] ) {
+			cnt++;
+			l++;
+		}
+		l++;
+		val = a[l-1];
+	}
+
+	if ( cnt == k && val >= 1 && val <= 1000000000 ) cout << val << "\n";
+	else cout << -1 << "\n";
 
 	return 0;
 }

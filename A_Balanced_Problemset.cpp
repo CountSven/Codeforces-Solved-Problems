c1b7773025e6f191f7afa15092743590bdefcf40
CountSven <abdullahalsakib001@gmail.com>
2026-03-07 07:08:08 +0600

    Add files via upload

--- A_Balanced_Problemset.cpp
+++ A_Balanced_Problemset.cpp
@@ -15,21 +15,17 @@ int main()
 
 		int res = 1;
 
-		if ( n == 1 ) cout << x << "\n";
-		else if ( 2 * n > x ) cout << res << "\n";
-		else {
-			for ( int i = 1; i * i <= x ; i++ ) {
-				if ( x % i ) continue;
+		for ( int i = 1; i * i <= x; i++ ) {
+			if ( x % i ) continue;
 
-				int val1 = i;
-				int val2 = x / i;
+			int div1 = x / i;
+			int div2 = x / div1;
 
-				if ( ( 1LL * ( n - 1 ) * val1 ) < x ) res = max( res, val1 );
-				if ( ( 1LL * ( n - 1 ) * val2 ) < x ) res = max( res, val2 );
-			}
-
-			cout << res << "\n";
+			if ( x / div1 >= n ) res = max( res, div1 );
+			if ( x / div2 >= n ) res = max( res, div2 ); 
 		}
+
+		cout << res << "\n";
 	}
 
 	return 0;

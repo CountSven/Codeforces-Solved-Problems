c703158f30254915ed17af16c9ad7351a40b35ed
CountSven <abdullahalsakib001@gmail.com>
2026-03-09 11:05:31 +0600

    Add files via upload

--- Retaliation.cpp
+++ Retaliation.cpp
@@ -12,35 +12,38 @@ int main()
 	while ( t-- ) {
 		int n;
 		cin >> n;
-		int a[n+1];
+		long long a[n+1];
 
 		for ( int i = 1; i <= n; i++ ) cin >> a[i];
 
-		set<int> st;
+		long long up = ( 2 * a[1] ) - a[2];
+		long long down = n + 1;
 
-		for ( int i = 2; i <= n; i++ ) st.insert( a[i] - a[i-1] );
-
-		if ( st.size() > 1 ) cout << "NO" << "\n";
-		else {
-			int diff = *st.begin();
-
-			for ( int i = 1; i <= n; i++ ) {
-				if ( diff > 0 ) a[i] -= ( diff * i );
-				else a[i] -= ( abs(diff) * ( n - i + 1 ) );
-			}
-
-			int f = 0;
-
-			for ( int i = 1; i <= n; i++ ) {
-				if ( a[i] < 0 || a[i] % ( n + 1 ) != 0 ) {
-					f = 1;
-					break;
-				}
-			}
-
-			if ( !f ) cout << "YES" << "\n";
-			else cout << "NO" << "\n";
+		if ( up < 0 || up % down ) {
+			cout << "NO" << "\n";
+			continue;
 		}
+
+		long long y = up / down;
+		
+		if ( y - a[1] + a[2] < 0 ) {
+			cout << "NO" << "\n";
+			continue;
+		}
+
+		long long x = y - a[1] + a[2];
+
+		int f = 0;
+		
+		for ( int i = 1; i <= n; i++ ) {
+			if ( a[i] - ( i * x ) - ( ( n - i + 1 ) * y ) != 0 ) {
+				f = 1;
+				break;
+			}
+		}
+
+		if ( !f ) cout << "YES" << "\n";
+		else cout << "NO" << "\n"; 
 	}
 
 	return 0;

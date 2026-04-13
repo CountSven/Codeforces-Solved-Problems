8eab4d07cfc4fb9e893542335846a2cea05431e4
CountSven <abdullahalsakib001@gmail.com>
2026-04-14 04:28:32 +0600

    Add files via upload

--- Common_Divisors.cpp
+++ Common_Divisors.cpp
@@ -9,37 +9,19 @@ int main()
 	int n;
 	cin >> n;
 
-	set<long long> st, divs;
+	long long x, g, cnt = 0;
 
 	for ( int i = 0; i < n; i++ ) {
-		long long x;
 		cin >> x;
-		st.insert( x );
+		if ( !i ) g = x;
+		else g = __gcd( g, x ); 
 	}
 
-	vector<long long> v;
-
-	for ( auto u : st ) v.push_back( u );
-
-	for ( long long i = 1; i * i <= v[0]; i++ ) {
-		if ( v[0] % i == 0 ) divs.insert( i );
-	}
-
-	// for ( auto u : divs ) cout << u << " ";
-	// cout << "\n";
-
-	long long cnt = 0;
-
-	for ( auto u : divs ) {
-		int f1 = 1, f2 = 1;
-		if ( u == ( v[0] / u ) ) f2 = 0;
-		for ( auto x : v ) {
-			if ( x % u ) f1 = 0;
-			if ( x % ( v[0] / u ) ) f2 = 0;
-			if ( !f1 && !f2 ) break;
+	for ( long long i = 1; i * i <= g; i++ ) {
+		if ( g % i == 0 ) {
+			if ( i != ( g / i ) ) cnt += 2;
+			else cnt++;
 		}
-		if ( f1 && f2 ) cnt += 2;
-		else if ( f1 || f2 ) cnt++;
 	}
 
 	cout << cnt << "\n";

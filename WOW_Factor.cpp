3657650d21d78f9d23917c635e5e41e6d120e87c
CountSven <abdullahalsakib001@gmail.com>
2026-04-24 14:11:13 +0600

    Add files via upload

--- WOW_Factor.cpp
+++ WOW_Factor.cpp
@@ -9,68 +9,18 @@ int main()
 	string s;
 	cin >> s;
 
-	vector<pair<int, char>> v;
+	long long l = 0, r = 0, res = 0;
 
-	int cnt = 0;
-	char c = s[0];
-
-	for ( char i : s ) {
-		if ( i != c ) {
-			v.push_back( { cnt, c } );
-			cnt = 1;
-			c = i;
-		}
-		else cnt++;
+	for ( int i = 1; i < s.size(); i++ ) {
+		if ( s[i] == 'v' && s[i-1] == 'v' ) r++;
 	}
 
-	v.push_back( { cnt, c } );
-
-	// for ( auto [x, y] : v ) cout << x << " " << y << "\n";
-
-	string cur = "";
-
-	for ( auto [x , y] : v ) {
-		if ( y == 'v' ) {
-			x--;
-			while ( x-- ) cur += 'w';
+	for ( int i = 1; i < s.size(); i++ ) {
+		if ( s[i] == 'v' && s[i-1] == 'v' ) {
+			l++;
+			r--;
 		}
-		else {
-			while ( x-- ) cur += 'o';
-		}
-	}
-
-	while ( cur.back() == 'o' ) cur.pop_back();
-	reverse( cur.begin(), cur.end() );
-	while ( cur.back() == 'o' ) cur.pop_back();
-	reverse( cur.begin(), cur.end() );
-
-	// cout << cur << "\n";
-
-	vector<pair<int, int>> chk;
-
-	int i = 0, cntW = 0, cntO = 0;
-
-	while ( i < cur.size() ) {
-		if ( cur[i] == 'w' ) {
-			while ( i < cur.size() && cur[i++] == 'w' ) cntW++;
-			chk.push_back( { cntW, cntO } );
-			cntO++;
-			cntW = 0;
-		}
-		else {
-			cntO++;
-			i++;
-		}
-	}
-
-	// for ( auto [x, y] : chk ) cout << x << " " << y << "\n";
-
-	long long res = 0, sumW = 0, sumWO = 0;
-
-	for ( auto [w, o] : chk ) {
-		res += ( 1LL * w * ( o * sumW - sumWO ) );
-		sumW += w;
-		sumWO += ( 1LL * w * o );
+		else if ( s[i] == 'o' ) res += ( l * r );
 	}
 
 	cout << res << "\n";

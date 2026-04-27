1c823bcb9b8c1ff967f49071151640b070d5a567
CountSven <abdullahalsakib001@gmail.com>
2026-04-27 16:51:27 +0600

    Add files via upload

--- Arrow_Path.cpp
+++ Arrow_Path.cpp
@@ -1,6 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int n, f = 0;
+char c[2][200005];
+int vis[2][200005][2];
+
+int dr[4] = { -1, 1, 0, 0 };
+int dc[4] = { 0, 0, -1, 1 };
+
+void dfs( int i, int j, int tp )
+{
+	if ( i == 1 && j == n-1 ) {
+		f = 1;
+		return;
+	}
+
+	vis[i][j][tp] = 1;
+
+	if ( tp == 0 ) {
+		for ( int cur = 0; cur < 4; cur++ ) {
+			int ni = i + dr[cur];
+			int nj = j + dc[cur];
+
+			if ( ni >= 0 && ni < 2 && nj >= 0 && nj < n ) {
+				if ( !vis[ni][nj][1] ) dfs( ni, nj, 1 );
+			}
+		}
+	}
+	else {
+		if ( c[i][j] == '>' ) {
+			if ( !vis[i][j+1][0] ) dfs( i, j+1, 0 );
+		}
+		else {
+			if ( !vis[i][j-1][0] ) dfs( i, j-1, 0 );
+		}
+	}
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
@@ -10,18 +46,23 @@ int main()
 	cin >> t;
 
 	while ( t-- ) {
-		int n;
-		string s1, s2;
-		cin >> n >> s1 >> s2;
+		cin >> n;
 
-		int f = 0;
-
-		for ( int i = 0; i+1 < n; i++ ) {
-			if ( i % 2 == 0 && s2[i] == '<' && s1[i+1] == '<' ) f = 1;
-			if ( i % 2 == 1 && s2[i+1] == '<' && s1[i] == '<' ) f = 1;
+		for ( int i = 0; i < 2; i++ ) {
+			for ( int j = 0; j < n; j++ ) cin >> c[i][j];
 		}
 
-		if ( !f ) cout << "YES" << "\n";
+		for ( int i = 0; i < 2; i++ ) {
+			for ( int j = 0; j < n; j++ ) {
+				for ( int k = 0; k < 2; k++ ) vis[i][j][k] = 0;
+			}
+		}
+
+		f = 0;
+
+		dfs( 0, 0, 0 );
+
+		if ( f ) cout << "YES" << "\n";
 		else cout << "NO" << "\n";
 	}
 

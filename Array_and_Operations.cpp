585d64ec2d9743c357c61b5c7bc44af0c9ae96ad
CountSven <abdullahalsakib001@gmail.com>
2026-04-29 17:10:12 +0600

    Add files via upload

--- Array_and_Operations.cpp
+++ Array_and_Operations.cpp
@@ -18,17 +18,11 @@ int main()
 
 		sort( v.rbegin(), v.rend() );
 
-		map<int, int> mp;
-
-		for ( int i = 0; i < ( 2 * k ); i++ ) mp[v[i]]++;
-
 		int res = 0;
-		
-		for ( auto [x, y] : mp ) {
-			if ( y > k ) res += y - k;
-		}
 
-		for ( int i = 2 * k; i < n; i++ ) res += v[i];
+		for ( int i = 0; i < k; i++ ) res += ( v[i+k] / v[i] );
+
+		for ( int i = 2*k; i < n; i++ ) res += v[i];
 
 		cout << res << "\n";		
 	}

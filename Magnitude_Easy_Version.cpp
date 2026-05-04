fe836d8db70a849f3622acf8694fab7000b72964
CountSven <abdullahalsakib001@gmail.com>
2026-05-04 17:41:22 +0600

    Add files via upload

--- Magnitude_Easy_Version.cpp
+++ Magnitude_Easy_Version.cpp
@@ -10,17 +10,30 @@ int main()
 	cin >> t;
 
 	while ( t-- ) {
-		long long n, x, mn = 0, mx = 0;
+		int n;
 		cin >> n;
+		int a[n];
+
+		for ( int i = 0; i < n; i++ ) cin >> a[i];
+
+		long long sum = 0, mn = a[0], mnPos = 0;
 
 		for ( int i = 0; i < n; i++ ) {
-			cin >> x;
-			mn += x;
-			mx += x;
-			mx = max( abs(mn), abs(mx) );
+			sum += a[i];
+			if ( sum < mn ) {
+				mn = sum;
+				mnPos = i;
+			}
 		}
 
-		cout << mx << "\n";
+		sum = 0;
+
+		for ( int i = 0; i < n; i++ ) {
+			sum += a[i];
+			if ( i == mnPos ) sum = abs( sum );
+		}
+
+		cout << sum << "\n";
 	}
 
 	return 0;

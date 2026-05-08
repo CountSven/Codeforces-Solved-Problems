752699cb590227a7b7a3f906dcf9024bde46997e
CountSven <abdullahalsakib001@gmail.com>
2026-05-08 11:17:59 +0600

    Add files via upload

--- Interesting_Ratio.cpp
+++ Interesting_Ratio.cpp
@@ -2,26 +2,28 @@
 using namespace std;
 
 const int rng = 1e7+5;
-vector<int> seive(rng, 0), prs;
+vector<bool> seive(rng, 1);
+vector<int> prs;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	for ( int i = 1; i <= rng-5; i++ ) {
-		for ( int j = i; j <= rng-5; j += i ) seive[j]++;
+	for ( int i = 2; i * i <= rng-5; i++ ) {
+		if ( !seive[i] ) continue;
+		for ( int j = i * i; j <= rng-5; j += i ) seive[j] = 0;
 	}
 
 	// for ( int i = 1; i <= 15; i++ ) cout << seive[i] << " ";
 	// cout << "\n";
 
-	for ( int i = 1; i <= rng-5; i++ ) {
-		if ( seive[i] == 2 ) prs.push_back( i );
+	for ( int i = 2; i <= rng-5; i++ ) {
+		if ( seive[i] ) prs.push_back( i );
 	}
 
 	// cout << prs.size() << "\n";
-	// for ( int i = 1; i <= 10; i++ ) cout << prs[i] << " ";
+	// for ( int i = 0; i < 10; i++ ) cout << prs[i] << " ";
 	// cout << "\n";
 
 	int t;

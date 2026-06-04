fcfa85387660a680ec6ac709eb008cf8a01d1b07
CountSven <abdullahalsakib001@gmail.com>
2026-06-04 19:37:06 +0600

    Add files via upload

--- Two_Buttons.cpp
+++ Two_Buttons.cpp
@@ -1,25 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int n, m;
+vector<int> dist( 2e4+5, -1 );
+queue<int> q;
+
+void bfs( int n )
+{
+	dist[n] = 0;
+
+	q.push( n );
+
+	while ( !q.empty() ) {
+		int u = q.front();
+		q.pop();
+
+		int v = 2 * u;
+
+		if ( v <= 2e4 && dist[v] == -1 ) {
+			dist[v] = dist[u] + 1;
+			q.push( v );
+		}
+
+		v = u - 1;
+
+		if ( v >= 1 && dist[v] == -1 ) {
+			dist[v] = dist[u] + 1;
+			q.push( v );
+		}
+	} 
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int n, m;
 	cin >> n >> m;
 
 	if ( n >= m ) cout << n - m << "\n";
 	else {
-		int cnt = 0;
-
-		while ( m > n ) {
-			if ( m % 2 ) m++;
-			else m /= 2;
-			cnt++;
-		}
-
-		cout << cnt + abs( m - n ) << "\n";
+		bfs( n );
+		cout << dist[m] << "\n";
 	}
 
 	return 0;

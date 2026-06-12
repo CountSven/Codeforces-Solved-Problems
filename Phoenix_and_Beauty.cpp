7e5a8da0684d0fde1e6700906b9139add3c0ac24
CountSven <abdullahalsakib001@gmail.com>
2026-06-12 21:52:38 +0600

    Add files via upload

--- Phoenix_and_Beauty.cpp
+++ Phoenix_and_Beauty.cpp
@@ -10,53 +10,28 @@ int main()
 	cin >> t;
 
 	while ( t-- ) {
-		int n, k, mx = 0;
+		int n, k;
 		cin >> n >> k;
-		int a[n];
 
-		map<int, int> mp;
+		set<int> st;
 
 		for ( int i = 0; i < n; i++ ) {
-			cin >> a[i];
-			mp[a[i]]++;
-			mx = max( mx, mp[a[i]] );
+			int x;
+			cin >> x;
+			st.insert( x );
 		}
 
-		if ( k == n || mp.size() == 1 ) {
-			cout << n << "\n";
-			for ( int i = 0; i < n; i++ ) cout << a[i] << " ";
-			cout << "\n";
-		}
-		else if ( k < (int)mp.size() ) cout << -1 << "\n";
+		if ( st.size() > k ) cout << -1 << "\n";
 		else {
-			vector<int> v, nv;
-			set<int> st;
+			vector<int> v;
 
+			for ( auto u : st ) v.push_back( u );
+			while ( v.size() < k ) v.push_back( 1 );
+
+			cout << n * k << "\n";
 			for ( int i = 0; i < n; i++ ) {
-				if ( st.find( a[i] ) == st.end() ) {
-					v.push_back( a[i] );
-					st.insert( a[i] );
-				}
+				for ( auto u : v ) cout << u << " ";
 			}
-
-			for ( int i = 1; i <= n; i++ ) {
-				if ( st.size() == k ) break;
-				if ( st.find( i ) == st.end() ) {
-					v.push_back( i );
-					st.insert( i );
-				}
-			}
-
-			mx = max( mx, k );
-			int cnt = 0;
-
-			while ( cnt < n ) {
-				for ( auto u : v ) nv.push_back( u );
-				cnt++;
-			}
-			
-			cout << nv.size() << "\n";
-			for ( auto u : nv ) cout << u << " ";
 			cout << "\n";
 		}
 	}

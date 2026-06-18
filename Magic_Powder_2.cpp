e31eb0e1645484a204d953ba535e344641f593a4
CountSven <abdullahalsakib001@gmail.com>
2026-06-18 23:19:00 +0600

    Add files via upload

---
+++ Magic_Powder_2.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = unsigned long long;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, k;
+	cin >> n >> k;
+	ll a[n], b[n];
+
+	for ( int i = 0; i < n; i++ ) cin >> a[i];
+	for ( int i = 0; i < n; i++ ) cin >> b[i];
+
+	ll l = 0, r = 2e16, res = 2e16;
+
+	while ( l <= r ) {
+		ll mid = l + ( r - l ) / 2;
+
+		ll tmp = k, f = 0;
+
+		for ( int i = 0; i < n; i++ ) {
+			ll val = mid * a[i];
+			ll need = 0;
+			if ( val > b[i] ) need = val - b[i];
+			if ( need > tmp ) {
+				f = 1;
+				break;
+			}
+			else tmp -= need;
+		}
+
+		if ( !f ) {
+			res = mid;
+			l = mid + 1;
+		}
+		else r = mid - 1;
+	}
+
+	cout << res << "\n";
+
+	return 0;
+}

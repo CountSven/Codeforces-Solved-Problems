d284ccf75e731d41d7ef5a1247e6a5c33e736460
CountSven <abdullahalsakib001@gmail.com>
2026-06-18 21:47:26 +0600

    Add files via upload

--- Rotation_Matching.cpp
+++ Rotation_Matching.cpp
@@ -8,30 +8,27 @@ int main()
 
 	int n;
 	cin >> n;
-	vector<int> a(n), b(n);
+	vector<int> a(n), b(n), v1(n), v2(n), v3(n);
 
-	map<int, int> mp1, mp2, mp3;
 
 	for ( int i = 0; i < n; i++ ) {
 		cin >> a[i];
 		a[i]--;
-		mp1[a[i]] = i;
+		v1[a[i]] = i;
 	}
 
 	for ( int i = 0; i < n; i++ ) {
 		cin >> b[i];
 		b[i]--;
-		mp2[b[i]] = i;
+		v2[b[i]] = i;
 	}
 
 	int mx = 0;
 
 	for ( int i = 0; i < n; i++ ) {
-		int cur = ( mp1[i] - mp2[i] + n ) % n;
-		mx = max( mx, ++mp3[cur] );
+		int cur = ( v1[i] - v2[i] + n ) % n;
+		mx = max( mx, ++v3[cur] );
 	}
-
-	// for ( auto [x, y] : mp3 ) cout << x << " " << y << "\n";
 
 	cout << mx << "\n";
 

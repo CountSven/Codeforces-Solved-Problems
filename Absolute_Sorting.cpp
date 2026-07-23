218b60dc657bf7ef2518a12f3a65f11fd641441a
CountSven <abdullahalsakib001@gmail.com>
2026-07-23 23:03:23 +0600

    Add files via upload

--- Absolute_Sorting.cpp
+++ Absolute_Sorting.cpp
@@ -14,34 +14,18 @@ int main()
 		cin >> n;
 		int a[n];
 
-		int l = 1e9, r = 0, x = -1;
+		for ( int i = 0; i < n; i++ ) cin >> a[i];
 
-		for ( int i = 0; i < n; i++ ) {
-			cin >> a[i];
-			l = min( l, a[i] );
-			r = max( r, a[i] );
+		int l = 0, r = 1e9;
+
+		for ( int i = 0; i+1 < n; i++ ) {
+			if ( a[i] == a[i+1] ) continue;
+			else if ( a[i] > a[i+1] ) l = max( l, ( a[i] + a[i+1] + 1 ) / 2 );
+			else r = min( r, ( a[i] + a[i+1] ) / 2 );
 		}
 
-		while ( l <= r ) {
-			int mid = ( l + r ) / 2, f = 0;
-
-			for ( int i = 0; i+1 < n; i++ ) {
-				if ( abs( a[i] - mid ) > abs( a[i+1] - mid ) ) {
-					if ( a[i] > a[i+1] ) f = 1;
-					else f = 2;
-					break;
-				}
-			}
-
-			if ( f == 1 ) l = mid + 1;
-			else if ( f == 2 ) r = mid - 1;
-			else {
-				x = mid;
-				break;
-			}
-		}
-
-		cout << x << "\n";
+		if ( l > r ) cout << -1 << "\n";
+		else cout << l << "\n";
 	}
 
 	return 0;

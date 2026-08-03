5b1863755abc38b4423d7d4e54cdc51fc7398db5
CountSven <abdullahalsakib001@gmail.com>
2026-08-03 18:59:48 +0600

    Add files via upload

--- GCD_sequence.cpp
+++ GCD_sequence.cpp
@@ -4,54 +4,47 @@ using namespace std;
 void solve() {
     int n;
     cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    vector<int> pref(n, 1), suf(n, 1);
+    vector<int> a(n), prefG(n, 0), suffG(n, 1e9), pGood(n, 1), sGood(n, 1);
 
-    for (int i = 2; i < n; i++) {
-        pref[i] = pref[i - 1];
-        if (__gcd(a[i - 2], a[i - 1]) > __gcd(a[i - 1], a[i])) {
-            pref[i] = 0;
-        }
-    }
-    for (int i = n - 3; i >= 0; i--) {
-        suf[i] = suf[i + 1];
-        if (__gcd(a[i], a[i + 1]) > __gcd(a[i + 1], a[i + 2])) {
-            suf[i] = 0;
-        }
+    for ( int i = 0; i < n; i++ ) cin >> a[i];
+
+    for ( int i = 1; i < n; i++ ) {
+        prefG[i] = __gcd( a[i], a[i-1] );
+        if ( prefG[i-1] > prefG[i] ) pGood[i] = 0;
+        pGood[i] = min( pGood[i-1], pGood[i] );
     }
 
+    for ( int i = n-2; i >= 0; i-- ) {
+        suffG[i] = __gcd( a[i], a[i+1] );
+        if ( suffG[i] > suffG[i+1] ) sGood[i] = 0;
+        sGood[i] = min( sGood[i+1], sGood[i] ); 
+    }
 
-    for (int i = 0; i < n; i++) {
-        vector<int> v;
-        for (int j = i - 2; j <= i + 2; j++) {
-            if (i != j && j >= 0 && j < n) {
-                v.push_back(a[j]);
+    if ( pGood[n-2] || sGood[1] ) {
+        cout << "YES" << "\n";
+        return;
+    }
+
+    for ( int i = 1; i+1 < n; i++ ) {
+        if ( pGood[i-1] && sGood[i+1] ) {
+            int g = __gcd( a[i-1], a[i+1] );
+            if ( prefG[i-1] <= g && g <= suffG[i+1] ) {
+                cout << "YES" << "\n";
+                return;
             }
         }
-        int res = (i == 0 || pref[i - 1]) & (i + 1 == n || suf[i + 1]);
-        for (int j = 2; j < v.size(); j++) {
-            if (__gcd(v[j - 2], v[j - 1]) > __gcd(v[j - 1], v[j])) {
-                res = 0;
-            }
-        }
-        if (res) {
-            cout << "YES" << "\n";
-            return;
-        }
     }
+
     cout << "NO" << "\n";
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(0);
     cin.tie(nullptr);
 
     int t;
     cin >> t;
-    while (t--) {
-        solve();
-    }
+
+    while (t--) solve();
 }

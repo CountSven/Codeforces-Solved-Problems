9a0c4d95d970db1d9fed8c25df63e97fb4de03d1
CountSven <abdullahalsakib001@gmail.com>
2026-08-03 19:16:09 +0600

    Add files via upload

--- GCD_sequence.cpp
+++ GCD_sequence.cpp
@@ -1,41 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n;
+vector<int> a(2e5 + 5);
+
+bool chk( int idx )
+{
+    vector<int> v;
+
+    for ( int i = 0; i < n; i++ ) {
+        if ( i != idx ) v.push_back( a[i] );
+    }
+
+    int last = 0;
+
+    for ( int i = 0; i+1 < v.size(); i++ ) {
+        int cur = __gcd( v[i], v[i+1] );
+        if ( last > cur ) return false;
+        last = cur;
+    }
+
+    return true;
+}
+
 void solve() {
-    int n;
     cin >> n;
-    vector<int> a(n), prefG(n, 0), suffG(n, 1e9), pGood(n, 1), sGood(n, 1);
 
     for ( int i = 0; i < n; i++ ) cin >> a[i];
 
-    for ( int i = 1; i < n; i++ ) {
-        prefG[i] = __gcd( a[i], a[i-1] );
-        if ( prefG[i-1] > prefG[i] ) pGood[i] = 0;
-        pGood[i] = min( pGood[i-1], pGood[i] );
+    int last = 0, idx = -1;
+
+    for ( int i = 0; i+1 < n; i++ ) {
+        int cur = __gcd( a[i], a[i+1] );
+        if ( last > cur ) {
+            idx = i;
+            break;
+        }
+        last = cur;
     }
 
-    for ( int i = n-2; i >= 0; i-- ) {
-        suffG[i] = __gcd( a[i], a[i+1] );
-        if ( suffG[i] > suffG[i+1] ) sGood[i] = 0;
-        sGood[i] = min( sGood[i+1], sGood[i] ); 
-    }
-
-    if ( pGood[n-2] || sGood[1] ) {
+    if ( idx == -1 ) {
         cout << "YES" << "\n";
         return;
     }
 
-    for ( int i = 1; i+1 < n; i++ ) {
-        if ( pGood[i-1] && sGood[i+1] ) {
-            int g = __gcd( a[i-1], a[i+1] );
-            if ( prefG[i-1] <= g && g <= suffG[i+1] ) {
-                cout << "YES" << "\n";
-                return;
-            }
-        }
-    }
-
-    cout << "NO" << "\n";
+    if ( chk( idx - 1 ) || chk( idx ) || chk( idx + 1 ) ) cout << "YES" << "\n";
+    else cout << "NO" << "\n";
 }
 
 int main()

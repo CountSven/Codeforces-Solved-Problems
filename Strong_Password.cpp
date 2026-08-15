ca5e01a78938f44cb164a82c8332cddaec1b4b1b
CountSven <abdullahalsakib001@gmail.com>
2026-08-15 21:46:22 +0600

    Add files via upload

--- Strong_Password.cpp
+++ Strong_Password.cpp
@@ -1,32 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void solve()
-{
-	string s, l, r;
-	int m;
-	cin >> s >> m >> l >> r;
-
-	int n = s.size(), cur = 0;
-
-	for ( int i = 0; i < m; i++ ) {
-		int st = l[i] - '0';
-		int en = r[i] - '0';
-		vector<int> first( 10, -1 );
-		for ( int j = cur; j < n; j++ ) {
-			if ( first[s[j] - '0'] == -1 ) first[s[j] - '0'] = j; 
-		}
-		for ( int j = st; j <= en; j++ ) {
-			if ( first[j] == -1 ) {
-				cout << "YES" << "\n";
-				return;
-			}
-			cur = max( cur, first[j] + 1 );
-		}
-	}
-	cout << "NO" << "\n";
-}
-
 int main()
 {
 	ios_base::sync_with_stdio(false);
@@ -35,7 +9,32 @@ int main()
 	int t;
 	cin >> t;
 
-	while ( t-- ) solve();
+	while ( t-- ) {
+		string s, l, r;
+		int m;
+		cin >> s >> m >> l >> r;
+
+		int n = s.size();
+
+		map<char, vector<int>> mp;
+
+		for ( int i = 0; i < n; i++ ) mp[s[i]].push_back( i );
+
+		int pos = -1;
+
+		for ( int i = 0; i < m; i++ ) {
+			int cur = pos;
+			for ( char j = l[i]; j <= r[i]; j++ ) {
+				auto it = upper_bound( mp[j].begin(), mp[j].end(), pos );
+				if ( it == mp[j].end() ) cur = n;
+				else cur = max( cur, *it );
+			}
+			pos = max( pos, cur );
+		}
+
+		if ( pos == n ) cout << "YES" << "\n";
+		else cout << "NO" << "\n";
+	}
 
 	return 0;
 }
